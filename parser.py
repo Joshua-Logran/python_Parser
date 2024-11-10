f27c6b1bdb91ab242de9a59999183e7083f6d8c1
Joshua Granados <axellogran03@gmail.com>
2024-11-10 16:30:50 -0600

    Aggregate Functions and Main Code Evaluation in C

--- parser.py
+++ parser.py
@@ -1,26 +1,38 @@
 import ply.lex as lex
 import ply.yacc as yacc
+
 # Symbol table for storing variables and types
 symbol_table = {}
 
-#Define the tokens
-tokens = ('KEYWORD', 'IDENTIFIER', 'CONSTANT', 'OPERATOR', 'LITERAL', 'PUNCTUATION', 'SPECIAL_CHARACTERS', 'SPACE', 'NEWLINE', 'COMMENT', 'OTHER')
-t_KEYWORD = r'auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|printf|include'
+# Define the tokens
+tokens = (
+    'LPAREN', 'RPAREN', 'KEYWORD', 'IDENTIFIER', 'NUMBER',
+    'OPERATOR', 'PUNCTUATION', 'INCLUDE', 'HEADER', 'STRING'
+)
+
+# Token definitions
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_KEYWORD = r'int|void|return|printf|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|printf|include'
 t_IDENTIFIER = r'[a-zA-Z_$][a-zA-Z_$0-9]*'
-t_OTHER = r'\s*<[^>]*>|\<.*?\>'
-t_OPERATOR = r'\+|\-|\*|\/|\%|\=\=|\!\=|\>|\>\=|\<|\<\=|\&\&|\|\||\=|\<\<|\>\>|\<\<\=|\>\>\=|\?|\:'
-t_COMMENT = r'\/\/.*'
-t_CONSTANT = r'\b[0-9]+\b|\d+'
-t_LITERAL = r'\".*?\"|\'.\''
-t_PUNCTUATION = r'\;|\,|\:|\[|\]|\(|\)|\{|\}'
-t_SPECIAL_CHARACTERS = r'\°|\#|\&'
+t_OPERATOR = r'\=|\=\=|\!\=|\>|\>\=|\<|\<\=|\&\&|\|\||\<\<|\>\>|\<\<\=|\>\>\=|\?|\:'
+t_PUNCTUATION = r';|,|\{|\}'  # Covers { and }
+t_INCLUDE = r'\#include'
+t_HEADER = r'\s*<[^>]*>|\<.*?\>'  # For headers like <stdio.h>
+t_STRING = r'\"([^\\"]|\\.)*\"'  # Capture strings, including escaped characters
 
-t_ignore = ' \t\n'
+# Ignored characters (whitespace)
+t_ignore = ' \t'
 
-# Define a rule so we can track line numbers
-def t_NEWLINE(t):
-    r'\n'
-    t.lexer.lineno += 1
+# Define a rule to track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Number token with integer values
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
     return t
 
 # Error handling rule
@@ -31,34 +43,99 @@ def t_error(t):
 # Build the lexer
 lexer = lex.lex()
 
-# Syntax Analysis (Parsing)
-    # Precedence and associativity of operators
-precedence = (
-    ('left', 'OPERATOR')
-)
+# Parsing rules
+precedence = ()  # No operator precedence needed for this structure
 
-# Define the grammar rules
+# The start rule for the parser
+def p_program(p):
+    '''program : include_statement function_definition'''
+    print("Program parsed successfully.")
+
+# Rule for include statements
+def p_include_statement(p):
+    '''include_statement : INCLUDE HEADER'''
+    #print(f"Include statement detected: {p[2]}")
+
+# Rule for function definition, including braces for the body
+def p_function_definition(p):
+    '''function_definition : KEYWORD IDENTIFIER LPAREN RPAREN PUNCTUATION statement_block'''
+    print("Function definition detected.")
+
+# Rule for a block of statements within curly braces
+def p_statement_block(p):
+    '''statement_block : PUNCTUATION statements PUNCTUATION'''
+    pass
+
+# Rule for multiple statements in function body
+def p_statements(p):
+    '''statements : statement statements
+                  | statement'''
+    pass
+
+# Rule for individual statements
+def p_statement(p):
+    '''statement : statement_declare
+                 | statement_assign
+                 | statement_printf
+                 | statement_return'''
+    pass
+
 # Rule for declaring variables
 def p_statement_declare(p):
-    'statement : KEYWORD IDENTIFIER PUNCTUATION'
+    '''statement_declare : KEYWORD IDENTIFIER PUNCTUATION'''
     var_type = p[1]
     var_name = p[2]
     if var_name in symbol_table:
-        print(f"Error semántico: La variable '{var_name}' ya está declarada.")
+        print(f"Semantic error: the variable '{var_name}' is already declared.")
     else:
         symbol_table[var_name] = var_type
-        print(f"Declaración detectada: {var_name} de tipo {var_type}")
+        print(f"Declaration detected: {var_name} of the type {var_type}")
 
 # Rule for assigning values to variables
 def p_statement_assign(p):
-    'statement : IDENTIFIER OPERATOR expression SEMICOLON'
+    '''statement_assign : IDENTIFIER OPERATOR NUMBER PUNCTUATION'''
     var_name = p[1]
     if var_name not in symbol_table:
-        print(f"Error semántico: La variable '{var_name}' no ha sido declarada.")
+        print(f"Semantic error: the variable '{var_name}' has not been declared yet.")
     else:
-        var_type = symbol_table[var_name]
-        expr_type = p[3][1]  # Tipo de la expresión
-        if var_type != expr_type:
-            print(f"Error semántico: No se puede asignar un valor de tipo '{expr_type}' a la variable '{var_name}' de tipo '{var_type}'.")
-        else:
-            print(f"Asignación válida: {var_name} = {p[3][0]}")
+        print(f"Valid assignment: {var_name} = {p[3]}")
+
+# Rule for printf statements
+def p_statement_printf(p):
+    '''statement_printf : KEYWORD LPAREN STRING RPAREN PUNCTUATION'''
+    print(f"Printf statement detected with message: {p[3]}")
+
+# Rule for return statements
+def p_statement_return(p):
+    '''statement_return : KEYWORD NUMBER PUNCTUATION'''
+    print(f"Return statement detected with value: {p[2]}")
+
+def p_error(p):
+    if p:
+        print(f"Syntax error at '{p.value}'")
+    else:
+        print("Syntax error at EOF")
+
+# Build the parser
+yacc.yacc()
+
+# Test lexer and parser with the input
+data = '''#include <stdio.h>
+int main() {
+    int a;
+    a = 5;
+    printf("Hola, mundo!\n");
+    return 0;
+}'''
+
+# Tokenize and parse the input
+lexer.input(data)
+while True:
+    token = lexer.token()
+    if not token:
+        break
+    print(token)
+
+# Parse
+result = yacc.parse(data)
+print(result)
